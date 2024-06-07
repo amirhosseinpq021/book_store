b19939cb2a7c767e3fb6f7e64146a75a8ca9470e
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-07 19:57:28 +0330

    delete password help text from signup
    we use this method :
    def __init__(self, *args, **kwargs):
            super().__init__(*args, **kwargs)
            self.fields['password1'].help_text = None
            self.fields['password2'].help_text = None

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,10 +8,14 @@ class CustomUserCreationForm(UserCreationForm):
         # fields = UserCreationForm.Meta.fields + ('age',)
         fields = ('username', 'email', 'first_name', 'last_name', 'age',)
 
+    def __init__(self, *args, **kwargs):  # delete password help text
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         # fields = UserChangeForm.Meta.fields
         fields = ('username', 'email', 'first_name', 'last_name', 'age',)
-
