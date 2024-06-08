f236d5175b9100bd9a29f43d4826fdb03f0cd801
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-08 12:45:35 +0330

    add models to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book, Category
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'category')
+    search_fields = ('title', 'author', 'price', 'category__category_name')
+    list_editable = ('price', 'category',)
+
+
+admin.site.register(Category)
+admin.site.register(Book, BookAdmin)
+
+
+
