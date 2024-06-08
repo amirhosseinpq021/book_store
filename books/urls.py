8804ca21dd304ab1e492c3f7cb505f82e5e3dbfd
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-08 14:21:51 +0330

    add BookDetail

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book_list'),
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
+
 ]
--- books/views.py
+++ books/views.py
@@ -9,3 +9,8 @@ class BookListView(generic.ListView):
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
+
+class BookDetail(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book_details'
