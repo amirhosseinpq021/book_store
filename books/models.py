0cb881023ef9ac4b022058d76116b2112bbc1569
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-08 12:29:33 +0330

    add method str to book model

--- books/models.py
+++ books/models.py
@@ -4,5 +4,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    content = models.TextField(max_length=2000)
+    description = models.TextField(max_length=2000)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+
