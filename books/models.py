d86b0ba67a31fa1e24fac7ca9f1102ef4a245a7f
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-08 12:32:38 +0330

    created category model

--- books/models.py
+++ books/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField(max_length=2000)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
