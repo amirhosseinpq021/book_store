4d2f5b2587a04db2b81a13a8ad17b289bc3d365c
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-09 16:13:27 +0330

    edit field price

--- books/models.py
+++ books/models.py
@@ -26,7 +26,7 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='cover/', blank=True)
     author = models.ForeignKey(Authors, on_delete=models.CASCADE)
     description = models.TextField(max_length=2000)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.PositiveIntegerField(max_length=20, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
